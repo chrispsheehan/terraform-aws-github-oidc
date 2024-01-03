42065ee05bff8cea12ebe0bf580c0c3be500308f
chrispsheehan <chrispsheehan@gmail.com>
2024-01-03 21:03:03 +0000

    allow all branches

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "terraform_validate_role" {
   role-name          = "${var.role-name-base}-gha-validate-role"
   actions            = local.terraform-init-actions
   resources          = var.resources
-  repo-ref           = var.repo
+  repo-ref           = "${var.repo}:*"
   github-oidc-domain = var.github-oidc-domain
 }
 
